f807da6106728109b6c6d51b34da0b8606fd67e1
masud-rana44 <mdmasudrana4488@gmail.com>
2023-08-28 21:43:28 +0600

    implemented folder structure

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+function layout({ children }: { children: React.ReactNode }) {
+  return <div className="h-full bg-indigo-100">{children}</div>;
+}
+
+export default layout;
---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <p className="text-3xl text-indigo-700 font-bold">Hello Discord clone</p>
+      <Button variant="default" size="lg">
+        Click Me
+      </Button>
+    </>
+  );
+}
