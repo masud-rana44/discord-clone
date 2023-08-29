77321741ec55859880bf03ba8580fcb3b8732d9b
masud-rana44 <mdmasudrana4488@gmail.com>
2023-08-30 02:29:10 +0600

    start implementing navigation side-bar

---
+++ components/navigation/navigation-item.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Image from "next/image";
+import { useParams, useRouter } from "next/navigation";
+
+import { cn } from "@/lib/utils";
+import { ActionTooltip } from "@/components/action-tooltip";
+
+interface NavigationItemProps {
+  id: string;
+  imageUrl: string;
+  name: string;
+}
+
+export const NavigationItem = ({ id, imageUrl, name }: NavigationItemProps) => {
+  return (
+    <ActionTooltip side="right" align="center" label={name}>
+      <div>Server</div>
+    </ActionTooltip>
+  );
+};
--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -4,6 +4,9 @@ import { db } from "@/lib/db";
 import { currentProfile } from "@/lib/current-profile";
 
 import { NavigationAction } from "./navigation-action";
+import { Separator } from "@/components/ui/separator";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { NavigationItem } from "./navigation-item";
 
 export const NavigationSidebar = async () => {
   const profile = await currentProfile();
@@ -23,6 +26,18 @@ export const NavigationSidebar = async () => {
   return (
     <div className="flex h-full w-full flex-col items-center space-y-4 py-3 text-primary dark:bg-[#1e1f22]">
       <NavigationAction />
+      <Separator className="mx-auto h-[2px] w-10 bg-zinc-300 dark:bg-zinc-700" />
+      <ScrollArea className="w-full flex-1">
+        {servers.map((server) => (
+          <div key={server.id}>
+            <NavigationItem
+              id={server.id}
+              name={server.name}
+              imageUrl={server.imageUrl}
+            />
+          </div>
+        ))}
+      </ScrollArea>
     </div>
   );
 };
