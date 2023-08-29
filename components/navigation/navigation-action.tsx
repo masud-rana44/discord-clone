d3c9c00e04c652396ba2c766de90e79b2e650c3e
masud-rana44 <mdmasudrana4488@gmail.com>
2023-08-30 02:54:32 +0600

    navigation item completed

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -7,7 +7,7 @@ export const NavigationAction = () => {
   return (
     <div>
       <ActionTooltip side="right" align="start" label="Add a server">
-        <button className="group">
+        <button className="group flex items-center">
           <div className="mx-3 flex h-[48px] w-[48px] items-center justify-center overflow-hidden rounded-[24px] bg-background transition-all group-hover:rounded-[16px] group-hover:bg-emerald-500 dark:bg-neutral-700">
             <Plus
               className="text-emerald-500 transition group-hover:text-white"
