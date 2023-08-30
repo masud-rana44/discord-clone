a1012511e034ae5068570c3e466182e7ed4e4d77
masud-rana44 <mdmasudrana4488@gmail.com>
2023-08-30 23:52:26 +0600

    implemented invite friends component

---
+++ components/modals/invite-modal.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { useState } from "react";
+import { Check, Copy, RefreshCw } from "lucide-react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { useModal } from "@/hooks/use-modal-store";
+import { Button } from "@/components/ui/button";
+import { useOrigin } from "@/hooks/use-origin";
+import axios from "axios";
+
+export const InviteModal = () => {
+  const { onOpen, isOpen, onClose, type, data } = useModal();
+  const origin = useOrigin();
+
+  const [copied, setCopied] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const isModalOpen = isOpen && type === "invite";
+  const { server } = data;
+
+  const inviteUrl = `${origin}/server/${server?.inviteCode}`;
+
+  const onCopy = () => {
+    navigator.clipboard.writeText(inviteUrl);
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
+
+  const onNew = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.patch(
+        `/api/servers/${server?.id}/invite-code`,
+      );
+
+      onOpen("invite", { server: response.data });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent className="overflow-hidden bg-white p-0 text-black">
+        <DialogHeader className="px-6 pt-8">
+          <DialogTitle className="text-bold text-center text-2xl">
+            Invite Friends
+          </DialogTitle>
+        </DialogHeader>
+        <div className="p-6">
+          <Label className="text-xs font-bold uppercase text-zinc-500 hover:text-secondary/70">
+            Server invite link
+          </Label>
+          <div className="mt-2 flex items-center gap-x-2">
+            <Input
+              disabled={isLoading}
+              className="border-0 bg-zinc-300/50 text-black focus-visible:ring-0 focus-visible:ring-offset-0"
+              value={inviteUrl}
+            />
+            <Button disabled={isLoading} size="icon" onClick={onCopy}>
+              {copied ? (
+                <Check className="h-4 w-4" />
+              ) : (
+                <Copy className="h-4 w-4" />
+              )}
+            </Button>
+          </div>
+          <Button
+            onClick={onNew}
+            disabled={isLoading}
+            variant="link"
+            size="sm"
+            className="text-xm mt-4 text-zinc-500"
+          >
+            Generate a new link
+            <RefreshCw className="ml-2 h-4 w-4" />
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -1,15 +1,6 @@
 "use client";
 
 import { MemberRole } from "@prisma/client";
-
-import { ServerWithMembersWithProfiles } from "@/types";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import {
   ChevronDown,
   LogOut,
@@ -20,12 +11,24 @@ import {
   Users,
 } from "lucide-react";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useModal } from "@/hooks/use-modal-store";
+import { ServerWithMembersWithProfiles } from "@/types";
+
 interface ServerHeaderProps {
   server: ServerWithMembersWithProfiles;
   role?: MemberRole;
 }
 
 export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+  const { onOpen } = useModal();
+
   const isAdmin = role === MemberRole.ADMIN;
   const isModerator = isAdmin || role === MemberRole.MODERATOR;
 
@@ -39,7 +42,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56 space-y-[2px] text-xs font-medium text-black dark:text-neutral-400">
         {isModerator && (
-          <DropdownMenuItem className="cursor-pointer px-3 py-2 text-xs text-indigo-600 dark:text-indigo-400">
+          <DropdownMenuItem
+            onClick={() => onOpen("invite", { server })}
+            className="cursor-pointer px-3 py-2 text-xs text-indigo-600 dark:text-indigo-400"
+          >
             Invite people <UserPlus className="ml-auto h-4 w-4" />
           </DropdownMenuItem>
         )}
