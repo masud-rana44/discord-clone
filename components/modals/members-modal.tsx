a3181237fb63af244c96b2cf51a285b6ab5bcba7
masud-rana44 <mdmasudrana4488@gmail.com>
2023-08-31 02:20:32 +0600

    start working manage member functionality

---
+++ components/modals/members-modal.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useModal } from "@/hooks/use-modal-store";
+import { ServerWithMembersWithProfiles } from "@/types";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { UserAvatar } from "@/components/user-avater";
+import { ShieldAlert, ShieldCheck } from "lucide-react";
+
+const roleIconMap = {
+  GUEST: null,
+  MODERATOR: <ShieldCheck className="ml-2 h-4 w-4 text-indigo-500" />,
+  ADMIN: <ShieldAlert className=" h-4 w-4 text-rose-500" />,
+};
+
+export const MembersModal = () => {
+  const { onOpen, isOpen, onClose, type, data } = useModal();
+
+  const isModalOpen = isOpen && type === "members";
+  const { server } = data as { server: ServerWithMembersWithProfiles };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent className="overflow-hidden bg-white  text-black">
+        <DialogHeader className="px-6 pt-8">
+          <DialogTitle className="text-bold text-center text-2xl">
+            Manage Members
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            {server?.members?.length} Members
+          </DialogDescription>
+        </DialogHeader>
+        <ScrollArea className="mt-8 max-h-[420px] pr-6">
+          {server?.members?.map((member) => (
+            <div key={member.id} className="mb-6 flex items-center gap-x-2">
+              <UserAvatar src={member.profile.imageUrl} />
+              <div className="flex flex-col gap-y-1">
+                <div className="flex items-center gap-x-1 text-xs font-semibold">
+                  {member.profile.name}
+                  {roleIconMap[member.role]}
+                </div>
+                <p className="text-xs text-zinc-500">{member.profile.email}</p>
+              </div>
+            </div>
+          ))}
+        </ScrollArea>
+      </DialogContent>
+    </Dialog>
+  );
+};
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -58,7 +58,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {isAdmin && (
-          <DropdownMenuItem className="cursor-pointer px-3 py-2 text-xs">
+          <DropdownMenuItem
+            onClick={() => onOpen("members", { server })}
+            className="cursor-pointer px-3 py-2 text-xs"
+          >
             Manage Members <Users className="ml-auto h-4 w-4" />
           </DropdownMenuItem>
         )}
