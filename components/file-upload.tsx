ec416b4c45cc651a215853c75df5a28ef099e7cd
masud-rana44 <mdmasudrana4488@gmail.com>
2023-09-04 01:42:41 +0600

    implement message attachment

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { X } from "lucide-react";
+import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 
 import { UploadDropzone } from "@/lib/uploadthing";
@@ -12,7 +12,7 @@ interface fileUploadProps {
   endpoint: "serverImage" | "messageFile";
 }
 
-const FileUpload = ({ endpoint, value, onChange }: fileUploadProps) => {
+export const FileUpload = ({ endpoint, value, onChange }: fileUploadProps) => {
   const fileType = value?.split(".").pop();
 
   if (value && fileType !== "pdf") {
@@ -35,6 +35,29 @@ const FileUpload = ({ endpoint, value, onChange }: fileUploadProps) => {
     );
   }
 
+  if (value && fileType === "pdf") {
+    return (
+      <div className="relative mt-2 flex items-center rounded-md bg-background/10 p-2">
+        <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-sm text-indigo-500 hover:underline dark:text-indigo-400"
+        >
+          {value}
+        </a>
+        <button
+          onClick={() => onChange("")}
+          className="absolute -right-2 -top-2 rounded-full bg-rose-500 p-1 text-white shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+
   return (
     <UploadDropzone
       endpoint={endpoint}
@@ -47,5 +70,3 @@ const FileUpload = ({ endpoint, value, onChange }: fileUploadProps) => {
     />
   );
 };
-
-export default FileUpload;
